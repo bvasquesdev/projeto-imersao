f6761695192e552c2eb04abf9634d51361d436ff
bvasquesdev <brunovasques96@gmail.com>
2024-09-04 16:41:38 -0300

    update projeto imersao

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Game of Thrones",
+        sinopse: "Game of Thrones é uma série de fantasia épica que cativou milhões de espectadores ao redor do mundo. Adaptada da saga literária 'As Crônicas de Gelo e Fogo', de George R.R. Martin, a trama se passa em um continente fictício chamado Westeros, onde famílias nobres travam uma guerra sangrenta pelo controle do Trono de Ferro e pelos Sete Reinos.",
+        link: "https://pt.wikipedia.org/wiki/Game_of_Thrones"
+    },
+    {
+        titulo: "Breaking Bad",
+        sinopse: "Breaking Bad é uma série dramática americana que acompanha a transformação de Walter White, um professor de química do ensino médio, em um poderoso chefão do tráfico de metanfetamina. Diagnosticado com câncer terminal, Walter decide garantir o futuro financeiro de sua família ao se envolver nesse perigoso mundo do crime.",
+        link: "https://pt.wikipedia.org/wiki/Breaking_Bad"
+    },
+    {
+        titulo: "House of Dragon",
+        sinopse: "A Casa do Dragão é um spin-off da aclamada série Game of Thrones, ambientado cerca de 200 anos antes dos eventos da série original. A trama se concentra na Casa Targaryen, no auge de seu poder, e narra uma sangrenta guerra civil conhecida como a Dança dos Dragões.",
+        link: "https://pt.wikipedia.org/wiki/Breaking_Bad"
+    }
+]
