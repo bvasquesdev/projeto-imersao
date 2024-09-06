7904cbb7da0145ffd5993efea4acb51420aa3ca1
bvasquesdev <brunovasques96@gmail.com>
2024-09-06 20:22:22 -0300

    projeto finalizado.

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 function pesquisar(){
     let section = document.getElementById("resultados-pesquisa");
     
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nenhuma série ou filme encontrado. Você precisa digitar algo válido.</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
     let resultados = "";
+    let titulo = "";
+    let sinopse = "";
+    let tags = "";
     
     for (let dado of dados) {
-        resultados += `
-         <div class="item-resultado">
-             <h2>${dado.titulo}</h2>
-                <p class="descricao-meta">${dado.sinopse}</p>
-                <a href="${dado.link}" target="_blank">Mais informações</a>
-         </div>  
-    `
-    }
+        titulo = dado.titulo.toLowerCase();
+        sinopse = dado.sinopse.toLowerCase();
+        tags = dado.tags.toLocaleLowerCase();
+        if (titulo.includes(campoPesquisa) || sinopse.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            resultados += `
+            <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                   <p class="descricao-meta">${dado.sinopse}</p>
+                   <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>  
+       `
+        };
+    };
     
-    section.innerHTML = resultados   
+    if (!resultados) {
+        resultados = "<p>Nenhuma série ou filme encontrado.</p>"
+    }
+
+    section.innerHTML = resultados;
 };
 //console.log(dados);
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo: "Game of Thrones",
         sinopse: "Game of Thrones é uma série de fantasia épica que cativou milhões de espectadores ao redor do mundo. Adaptada da saga literária 'As Crônicas de Gelo e Fogo', de George R.R. Martin, a trama se passa em um continente fictício chamado Westeros, onde famílias nobres travam uma guerra sangrenta pelo controle do Trono de Ferro e pelos Sete Reinos.",
-        link: "https://pt.wikipedia.org/wiki/Game_of_Thrones"
+        link: "https://pt.wikipedia.org/wiki/Game_of_Thrones",
+        tags: "got, hbo, jon snow, daenerys"
     },
     {
         titulo: "Breaking Bad",
         sinopse: "Breaking Bad é uma série dramática americana que acompanha a transformação de Walter White, um professor de química do ensino médio, em um poderoso chefão do tráfico de metanfetamina. Diagnosticado com câncer terminal, Walter decide garantir o futuro financeiro de sua família ao se envolver nesse perigoso mundo do crime.",
-        link: "https://pt.wikipedia.org/wiki/Breaking_Bad"
+        link: "https://pt.wikipedia.org/wiki/Breaking_Bad",
+        tags: "jesse pinkman, jesse, quimica, heisenberg"
     },
     {
         titulo: "House of Dragon",
         sinopse: "A Casa do Dragão é um spin-off da aclamada série Game of Thrones, ambientado cerca de 200 anos antes dos eventos da série original. A trama se concentra na Casa Targaryen, no auge de seu poder, e narra uma sangrenta guerra civil conhecida como a Dança dos Dragões.",
-        link: "https://pt.wikipedia.org/wiki/House_of_the_Dragon"
+        link: "https://pt.wikipedia.org/wiki/House_of_the_Dragon",
+        tags: "dragoes, rhaenyra , alicent, hbo"
     }
 ];
